31edb9e2a22aed4dc0567388268c6388c1feb799
Andrey <andrey@zavrichko.dev>
2021-01-17 13:33:24 +0200

    finish cursor

--- app/js/main.js
+++ app/js/main.js
@@ -54,21 +54,21 @@ $(function () {
 
   // Hamburger
   $(".hamburger").click(function () {
-    $(this).toggleClass('on')
+    $(this).toggleClass("on");
     $(".header__top").toggleClass("off");
     $(".sales-leaders__discount").toggleClass("off");
     $(".new-items__discount").toggleClass("off");
     $(".slick-arrow").toggleClass("off");
-  })
+  });
 
   // Sidebar
-  $('ul.main-menu li').click(function (e) {
+  $("ul.main-menu li").click(function (e) {
     e.preventDefault();
 
-    if ($(this).siblings('li').find('ul.sidebar__submenu:visible').length) {
-      $('ul.sidebar__submenu').slideUp('normal');
+    if ($(this).siblings("li").find("ul.sidebar__submenu:visible").length) {
+      $("ul.sidebar__submenu").slideUp("normal");
     }
-    $(this).find('ul.sidebar__submenu').slideToggle('normal');
+    $(this).find("ul.sidebar__submenu").slideToggle("normal");
   });
 
   var t1 = new TimelineMax({ paused: true });
@@ -78,12 +78,12 @@ $(function () {
   });
 
   t1.staggerFrom(
-    '.main-menu li a:not(.sidebar__submenu li a)',
+    ".main-menu li a:not(.sidebar__submenu li a)",
     1,
     {
       opacity: 0,
       y: 10,
-      ease: Power3.easeInOut
+      ease: Power3.easeInOut,
     },
     0.1
   );
@@ -91,14 +91,68 @@ $(function () {
   t1.from(".sidebar__submenu", 0.3, {
     autoAlpha: 0,
   });
-  
+
   t1.reverse();
 
-  $(document).on('click', '.menu-btn', function () {
+  $(document).on("click", ".menu-btn", function () {
     t1.reversed(!t1.reversed());
   });
 
-    $(document).on('click', '.close-menu', function () {
+  $(document).on("click", ".close-menu", function () {
     t1.reversed(!t1.reversed());
+  });
+
+  // Cursor
+  var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+  var posX = 0,
+    posY = 0,
+    mouseX = 0,
+    mouseY = 0;
+
+  TweenMax.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function () {
+      posX += (mouseX - posY) / 9;
+      posY += (mouseY - posY) / 9;
+
+      TweenMax.set(follower, {
+        css: {
+          left: posX - 20,
+          top: posY - 20,
+        },
+      });
+
+      TweenMax.set(cursor, {
+        css: {
+          left: mouseX - 65,
+          top: mouseY - 65,
+        },
+      });
+    },
+  });
+
+  $(document).on("mousemove", function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  });
+
+  $(".js-slider").on("mouseenter", function () {
+    cursor.addClass("active");
+    follower.addClass("active");
+  });
+
+  $(".js-slider").on("mouseleave", function () {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+  });
+
+  $(".js-slider-btn").on("mouseenter", function () {
+    cursor.addClass("hoverBtn");
+  });
+
+  $(".js-slider-btn").on("mouseleave", function () {
+    cursor.removeClass("hoverBtn");
   });
 });
